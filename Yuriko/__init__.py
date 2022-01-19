69192dc0974ed41c19a1eb64ca6ed4ff91fe49ee
santhu113 <95135053+santhu113@users.noreply.github.com>
2022-01-19 21:30:12 +0530

    Update __init__.py

--- Yuriko/__init__.py
+++ Yuriko/__init__.py
@@ -282,7 +282,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from Yuriko.modules.helper_funcs.handlers import (
+from Santhu.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
